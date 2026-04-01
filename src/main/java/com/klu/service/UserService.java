303e61735d6de7e5de60890cb814ccfc2aec072d
ilyyy <2400030895@kluniversity.in>
2026-04-01 14:38:52 +0530

    Update UserService.java

--- src/main/java/com/klu/service/UserService.java
+++ src/main/java/com/klu/service/UserService.java
@@ -1,13 +1,19 @@
 package com.klu.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.klu.model.User;
 import com.klu.repository.UserRepository;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -22,9 +28,10 @@ public class UserService {
     public User login(String email, String password) {
         User user = userRepository.findByEmail(email);
 
-        if(user != null && user.getPassword().equals(password)) {
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
             return user;
         }
+
         return null;
     }
 }
